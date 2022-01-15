9e747302362c0686f50f1b19cea51085e732d42f
cypri3 <cypri3.jules@free.fr>
2022-01-15 16:52:57 +0100

    Shop fonctionnels

--- src/gameObjects/Entities/EntityItem.java
+++ src/gameObjects/Entities/EntityItem.java
@@ -7,6 +7,7 @@ import libraries.Vector2;
  * @brief Classe représentant l'objet item, consommable par le joueur
  */
 public abstract class EntityItem extends Entity {
+    private int price;
     /**
      * 
      * @param pos
@@ -23,4 +24,21 @@ public abstract class EntityItem extends Entity {
      */
     public void onHeroItemAction(Hero h) {
     }
+
+    /**
+     * Définit le prix d'un objet
+     * @param price = prix de l'objet
+     */
+    public void setPrice(int price){
+        this.price = price;
+    }
+
+    /**
+     * Donne le prix d'un objet
+     * @return le prix
+     */
+    public int getPrice(){
+        return this.price;
+    }
+
 }
