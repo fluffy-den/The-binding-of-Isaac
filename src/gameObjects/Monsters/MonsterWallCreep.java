1dfd933e3aa67e5866cb68204a5d7eec14d6e18e
Fluffy <noe.louis-quentin@hotmail.fr>
2022-01-16 16:34:42 +0100

    Ajout génération de projectiles dans un cercle,

--- src/gameObjects/Monsters/MonsterWallCreep.java
+++ src/gameObjects/Monsters/MonsterWallCreep.java
@@ -2,11 +2,19 @@ package gameObjects.Monsters;
 
 import gameAI.AI;
 
-import gameObjects.Entities.EntityMonster;
+import java.util.List;
+import java.util.LinkedList;
 
+import gameObjects.Hero;
+import gameObjects.Entities.EntityMonster;
+import gameObjects.Projectiles.MonsterHeavyProjectile;
+import gameObjects.Projectiles.MonsterLightProjectile;
+import gameObjects.Projectiles.MonsterProjectile;
+import gameWorld.GameCounter;
 import gameWorld.GameRoom;
 
 import libraries.Vector2;
+import resources.Utils;
 
 /**
  * 
@@ -15,12 +23,15 @@ public class MonsterWallCreep extends EntityMonster {
     public static final Vector2 SIZE = GameRoom.TILE_SIZE.scalarMultiplication(0.75)
             .vectorMultiplication(new Vector2(1.0, 0.7));
     public static final double SPEED = 0.002;
+    public static final double RELOAD_SPEED = 0.01;
     public static final double MELEE_RELOAD_SPEED = 0.040;
     public static final double MELEE_EFFECT_POWER = 5.;
     public static final double AGGRO_RANGE = GameRoom.TILE_DIST * 5.0;
     public static final int MELEE_DAMAGE = 4;
     public static final int HP = 12;
     public static final String IMGPATH = "images/WallCreep.png";
+
+    private GameCounter reloadCounter;
 
     /**
      * 
@@ -37,5 +48,17 @@ public class MonsterWallCreep extends EntityMonster {
                 MELEE_RELOAD_SPEED,
                 IMGPATH,
                 new AI(pos, AGGRO_RANGE));
+
+        this.reloadCounter = new GameCounter(RELOAD_SPEED);
+    }
+
+    /**
+     * 
+     */
+    public List<MonsterProjectile> fireProjectiles(Hero h) {
+        if (this.reloadCounter.isFinished()) {
+            return MonsterLightProjectile.generateProjectilesInCircle(this.pos, h, 8);
+        }
+        return null;
     }
 }
--- src/gameObjects/Monsters/MonsterWizoob.java
+++ src/gameObjects/Monsters/MonsterWizoob.java
@@ -2,10 +2,19 @@ package gameObjects.Monsters;
 
 import gameAI.AI;
 
+import java.util.List;
+import java.util.LinkedList;
+
+import gameObjects.Hero;
 import gameObjects.Entities.EntityMonster;
+import gameObjects.Projectiles.MonsterHeavyProjectile;
+import gameObjects.Projectiles.MonsterLightProjectile;
+import gameObjects.Projectiles.MonsterProjectile;
+import gameWorld.GameCounter;
 import gameWorld.GameRoom;
 
 import libraries.Vector2;
+import resources.Utils;
 
 /**
  * 
@@ -14,12 +23,15 @@ public class MonsterWizoob extends EntityMonster {
     public static final Vector2 SIZE = GameRoom.TILE_SIZE.scalarMultiplication(0.75)
             .vectorMultiplication(new Vector2(1.0, 0.7));
     public static final double SPEED = 0.0035;
+    public static final double RELOAD_SPEED = 0.01;
     public static final double MELEE_RELOAD_SPEED = 0.025;
     public static final double MELEE_EFFECT_POWER = 7.5;
     public static final double AGGRO_RANGE = GameRoom.TILE_DIST * 5.0;
     public static final int MELEE_DAMAGE = 1;
     public static final int HP = 8;
     public static final String IMGPATH = "images/Wizoob.png";
+
+    private GameCounter reloadCounter;
 
     /**
      * 
@@ -36,5 +48,21 @@ public class MonsterWizoob extends EntityMonster {
                 MELEE_RELOAD_SPEED,
                 IMGPATH,
                 new AI(pos, AGGRO_RANGE));
+
+        this.reloadCounter = new GameCounter(RELOAD_SPEED);
+    }
+
+    /**
+     * 
+     */
+    public List<MonsterProjectile> fireProjectiles(Hero h) {
+        if (this.reloadCounter.isFinished()) {
+            List<MonsterProjectile> pL = new LinkedList<MonsterProjectile>();
+            pL.add(new MonsterHeavyProjectile(
+                    this.pos,
+                    h.getPos().subVector(this.pos)));
+            return pL;
+        }
+        return null;
     }
 };
